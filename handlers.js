334cc5e9ea035b1297c59e4e72fd2bae0018178c
gjgd <gjgd@protonmail.com>
2020-06-22 19:00:14 +0200

    Add retry logic to avoid collision

--- handlers.js
+++ handlers.js
@@ -1,10 +1,44 @@
 const { DynamoDB } = require('aws-sdk');
 const uuid = require('uuid');
 const fs = require('fs');
+const retry = require('async-retry');
 
 const homeHtml = fs.readFileSync('./index.html').toString();
 
 const dynamoDb = new DynamoDB.DocumentClient();
+
+// eslint-disable-next-line arrow-body-style
+const getUniqueId = async () => {
+  // Retry 3 times in case of collision, then return error
+  return retry(
+    async (bail) => {
+      // FIXME ensure uniqueness
+      // https://stackoverflow.com/questions/11721308/how-to-make-a-uuid-in-dynamodb
+      // https://stackoverflow.com/questions/742013/how-do-i-create-a-url-shortener
+      const id = uuid.v4().split('-')[0];
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+          id,
+        },
+      };
+      const record = await dynamoDb.get(params).promise();
+      if (record.Item) {
+        const errorText = `${id} has a collision with record ${JSON.stringify(
+          record,
+          null,
+          2,
+        )}`;
+        console.error(errorText);
+        return bail(new Error(errorText));
+      }
+      return id;
+    },
+    {
+      retries: 3,
+    },
+  );
+};
 
 module.exports.home = async () => {
   console.log('home');
@@ -42,19 +76,11 @@ module.exports.redirect = async (event) => {
   };
 };
 
-const getNewId = async () => {
-  // FIXME ensure uniqueness
-  // https://stackoverflow.com/questions/11721308/how-to-make-a-uuid-in-dynamodb
-  // https://stackoverflow.com/questions/742013/how-do-i-create-a-url-shortener
-  const id = uuid.v4();
-  return id.split('-')[0];
-};
-
 module.exports.create = async (event) => {
   console.log('create');
   const body = JSON.parse(event.body);
   const { url } = body;
-  const id = await getNewId();
+  const id = await getUniqueId();
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
